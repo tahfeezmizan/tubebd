d0d55998fc953dc430a3f5d3e981d772faf12d1a
tahfeezmizan <mizanahmed5316@gmail.com>
2023-12-24 16:32:19 +0600

    message

---
+++ conversion.js
@@ -0,0 +1,14 @@
+let onionPrice = "1012";
+let pomegranatePrice = 300;
+let Result = onionPrice + pomegranatePrice;
+
+// console.log(Result);
+
+let num = parseInt(onionPrice);
+
+console.log(onionPrice);
+console.log(typeof(num));
+
+let number = 50.564;
+
+console.log(parseFloat(number))
